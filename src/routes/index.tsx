be1196370255b01446a1859b3c52cf0f3588b6aa
hajikazuo <hajikazuo@outlook.com>
2025-04-07 16:07:40 -0300

    feat slices e pages

---
+++ src/routes/index.tsx
@@ -0,0 +1,41 @@
+import { Route, Routes } from "react-router-dom"
+import { Auth } from "../pages/Auth"
+import { Layout } from "../components/Layout"
+import { Home } from "../pages/Home"
+import { NewTransaction } from "../pages/Transaction/New"
+import { EditTransaction } from "../pages/Transaction/Edit"
+
+export const MainRoutes = () => {
+    return (
+        <Routes>
+            <Route
+                path="/signin"
+                element={<Auth type="signin" />}
+            />
+
+            <Route
+                path="/signup"
+                element={<Auth type="signup" />}
+            />
+
+            <Route element={<Layout />}>
+                <Route
+                    index
+                    element={<Home />}
+                />
+
+                <Route path="/transacoes">
+                    <Route
+                        path="nova"
+                        element={<NewTransaction />}
+                    />
+
+                    <Route
+                        path="editar/:id"
+                        element={<EditTransaction />}
+                    />
+                </Route>
+            </Route>
+        </Routes >
+    )
+}
